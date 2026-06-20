f305f0516e17e40d0e443dc80f7e39a48b2fd425
Daviano <daviano123@gmail.com>
2026-06-20 21:00:30 +0700

    Initial commit

---
+++ hari1.py
@@ -0,0 +1,13 @@
+print("hello world")
+
+#Variabel & Assignment
+greeting = "selamat datang di kelas"
+print(greeting)
+
+addition = 2+2
+result = addition - 1
+print(result)
+
+#Input
+name = input("Masukkan nama: ")
+print("Hello", name)
