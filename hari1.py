d5c7d72e4cb16429832143161ffdc7dc79f03440
Daviano <daviano123@gmail.com>
2026-06-20 21:21:57 +0700

    nambah lagi

--- hari1.py
+++ hari1.py
@@ -11,3 +11,13 @@ print(result)
 #Input
 name = input("Masukkan nama: ")
 print("Hello", name)
+
+
+#Ujian TODO: Silakan buat kode Anda di bawah ini.
+greeting = ("Saya mulai belajar Python!")
+
+age = 17
+print(type(age))  
+
+salary = 5000000.0
+print(type(salary))
